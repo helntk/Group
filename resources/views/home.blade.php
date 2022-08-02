e085ea9b902601917117d389e7d4db3022d9c8bc
estevooliveira111 <carpedin37838691@gmail.com>
2022-08-02 19:09:49 -0300

    banner presentation

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<x-guest-layout>
+    
+    <!-- This example requires Tailwind CSS v2.0+ -->
+<div class="bg-gray-700">
+    <div class="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
+      <h2 class="text-3xl font-extrabold text-white sm:text-4xl">
+        <span class="block">Sistema em Desenvolvimento<span>
+      </h2>
+      <p class="mt-4 text-lg leading-6 text-gray-200">Vulnerabilidades, Ideias de Criação entre no link a seguir...</p>
+      <a href="https://github.com/estevooliveira111/Group" class="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-gray-600 bg-white hover:bg-gray-50 sm:w-auto">Link do Repositório</a>
+    </div>
+  </div>  
+
+</x-guest-layout>
+
+
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers;
 
-Route::view('', 'welcome')->name('home');
+Route::view('', 'home')->name('home');
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');    
 });
