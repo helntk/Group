1da5bb51ac6b9e865a31bdead4f197b1a97577a3
estevooliveira111 <carpedin37838691@gmail.com>
2022-08-02 18:29:22 -0300

    ...

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
 
-
-Route::get('/', function () {
-    return view('welcome');
+Route::view('', 'welcome')->name('home');
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+    Route::view('dashboard', 'dashboard')->name('dashboard');    
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
